26bc29e8300cad13d25e8ce88af22d5dc05d7e7b
めだか <kanten520212@gmail.com>
2021-01-25 09:58:34 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * kadai21-22.cpp
+ *
+ * Created: 2021/01/25 9:55:28
+ * Author : kante
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1){
+    }
+}
+
