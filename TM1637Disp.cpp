57b971ea1ac84ffdd601988a1ee771d5b233abcf
めだか <kanten520212@gmail.com>
2021-01-25 10:09:53 +0900

    2021.01.25

---
+++ TM1637Disp.cpp
@@ -0,0 +1,128 @@
+/****************************************************************************/
+/*	TM1637 + 4degit 7Seg LED Display Base Class								*/
+/*																			*/
+/*	本ファイルは，TM1637+4Digit 7Seg LEDに情報を表示するためのBase Class を定義	*/
+/*	したものである．															*/
+/*																			*/
+/*	■制約事項																*/
+/*		・TM1637Disp関数はCLK及びDIOが同一PORTにある場合のみ利用可能である．		*/
+/*		・TM1637Disp関数は非リエントラントである．								*/
+/*		・ライセンスはGPLに準じるものとする．									*/
+/*																			*/
+/*											東京国際工科専門職大学　荒金匡德	*/
+/****************************************************************************/
+/* #000		2021/01/08 初版										荒金匡德		*/
+/*																			*/
+/****************************************************************************/
+#include "TM1637Disp.h"
+
+// コンストラクタ
+TM1637Disp::TM1637Disp(void){
+	TM_DDR |= CLK|DIO;				// CLKポートDIOポートを出力に設定
+	// 7セグLED表示パターンデフォルト値設定（ 0-9, Blank ）
+	this->segPattern[0]  = 0x3F;			// 0
+	this->segPattern[1]  = 0x06;			// 1
+	this->segPattern[2]  = 0x5B;			// 2
+	this->segPattern[3]  = 0x4F;			// 3
+	this->segPattern[4]  = 0x66;			// 4
+	this->segPattern[5]  = 0x6D;			// 5
+	this->segPattern[6]  = 0x7D;			// 6
+	this->segPattern[7]  = 0x07;			// 7
+	this->segPattern[8]  = 0x7F;			// 8
+	this->segPattern[9]  = 0x6F;			// 9
+	this->segPattern[10] = 0x00;			// Blank
+
+}
+
+///////////////////////////////////////////////////////////
+//
+//    TM1637 4桁7セグメントLED表示用関数
+//    呼出形式：void		num_disp( UCHR *num);
+//
+//    引数：UCHR *num  :  表示データが入ったデータの先頭アドレス
+//                        (表示データは4バイト分の表示コード)
+//    戻り値：なし
+//
+//　　機能：引数で受け取った4バイトのメモリに入っているデータを
+//         4桁7セグメントLEDに表示する．
+//         但し，上記表示文字に.を付加する場合は，コードに0x80を
+//         加算する．
+//
+///////////////////////////////////////////////////////////
+void	TM1637Disp::numDisp( UCHR *num){
+
+	TM_DDR |= CLK|DIO;								// CLKポートDIOポートを出力に設定
+	Start_bit();									// Start bit 送信
+	send(MODE);										// モード送信
+	Stop_bit();										// Stop bit 送信
+
+	for( int i = 0; i < 4; i++){					// 4桁分のLoop
+		Start_bit();								// START Bit 送信
+		send(ADDR + i);								// 表示エリアのアドレス送信
+		if( *num >= 0x80){							// ドットを付加する場合
+			send(this->segPattern[(int)(*num-0x80)]|0x80);// ドットのセグメントを付加した情報を送信
+			} else {								// ドットを付加しない場合
+			send(this->segPattern[(int)*num]);			// ドットなしの表示情報を送信
+		}
+		Stop_bit();									// STOP Bit 送信
+		num++;										// 表示桁を進める
+	}
+
+	Start_bit();									// STOP Bit 送信
+	send(DCNT);										// Display Control Command 送信
+	Stop_bit();										// STOP Bit 送信
+}
+
+
+///////////////////////////////////////////////////////////
+//		START Bit 送信関数
+///////////////////////////////////////////////////////////
+void	TM1637Disp::Start_bit( void){
+	CLK_H;											// CLKをHIGH
+	_delay_us(DLY);
+	DIO_H;											// DIOをHIGH
+	_delay_us(DLY);
+	DIO_L;											// DIOをLOW
+	_delay_us(DLY);
+}
+
+///////////////////////////////////////////////////////////
+//		STOP Bit 送信関数
+///////////////////////////////////////////////////////////
+void	TM1637Disp::Stop_bit( void){
+	DIO_L;											// DIOをLOW
+	_delay_us(DLY);
+	CLK_H;											// CLKをHIGH
+	_delay_us(DLY);
+	DIO_H;											// DIOをHIGH
+	_delay_us(DLY);
+}
+
+///////////////////////////////////////////////////////////
+//		Byte data 送信関数
+///////////////////////////////////////////////////////////
+void	TM1637Disp::send( UCHR	data){
+	for( int i=0; i<8; i++){						// 8Bitデータ送出なので8回Loopする．
+		CLK_L;										// CLKにLOWを出力
+		_delay_us(DLY);
+		if( (data & 0x01) == 0){					// 出力データが0の時
+			DIO_L;									//　 DIOにLOWを出力
+		} else{										// 出力データが1の時
+			DIO_H;									// 　DIOにHIGHを出力
+		}
+		_delay_us(DLY);
+		CLK_H;										// CLKにHIGHを出力
+		_delay_us(DLY);
+		data >>= 1;									// 出力データを1ビット右シフト
+	}
+	// ACK待ち
+	CLK_L;
+	_delay_us(DLY);
+	// CLKをHIGHにしてACK解除
+	CLK_H;											// CLKにHIGHを出力
+	_delay_us(DLY);
+	DIO_L;											// DIOにLOWを出力
+	_delay_us(DLY);
+	CLK_L;											// CLKにLOWを出力
+	_delay_us(DLY);
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,55 @@
 /*
- * kadai21-22.cpp
- *
- * Created: 2021/01/25 9:55:28
- * Author : kante
+ * main.cpp
+ * 温湿度計サンプルプログラム
+ * Created: 2021/01/25 10:06:00
+ * Author : MorimotoYuma
  */ 
 
 #include <avr/io.h>
-
+#include "TM1637Disp.h"
+#include "DHT11.h"
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1){
+		// 7セグLED表示パターン（0-9，°,C,o ）
+		static UCHR	ptn[14] = { 0x3F,	// 0:0
+								0x06,	// 1:1
+								0x5B,	// 2:2
+								0x4F,	// 3:3
+								0x66,	// 4:4
+								0x6D,	// 5:5
+								0x7D,	// 6:6
+								0x07,	// 7:7
+								0x7F,	// 8:8
+								0x6F,	// 9:9
+								0x63,	// °:10
+								0x39,	// C:11
+								0x5C,	// o:12
+								0xFF};	// Terminate
+	UCHR		dispData[4];								// 表示用データバッファ
+	TM1637Disp	disp(&ptn[0]);								// 4桁7Segインスタンス生成
+	DHT11		TandH;										// 温湿度センサインスタンス生成
+	_delay_ms(2000);										// 電源ON後2秒間待つ
+	
+    while (1) {
+		if( !TandH.DHT11Read()){							// 温湿度センサから値を取得（成功）
+			dispData[0] = TandH.getTemperature_H() / 10;	// 温度10の位の値取得
+			dispData[1] = TandH.getTemperature_H() %10;		// 温度1の位の値取得
+			dispData[2] = 10;								// 温度の単位表示
+			dispData[3] = 11;								// 温度の単位表示
+			disp.numDisp(dispData);							// 7Segに表示
+			_delay_ms(2000);								// 2秒待つ
+			dispData[0] = TandH.getHumidity_H() / 10;		// 湿度10の位の値取得
+			dispData[1] = TandH.getHumidity_H() %10;		// 湿度1の位の値取得
+			dispData[2] = 10;								// 湿度の単位表示
+			dispData[3] = 12;								// 湿度の単位表示
+			disp.numDisp(dispData);							// 7Segに表示
+			_delay_ms(2000);								// 2秒待つ
+		} else {											// 温湿度センサからの値取得失敗
+			dispData[0] = dispData[1] = dispData[2] = dispData[3] = 0x80;// 「....」を表示データとしてセット
+			disp.numDisp(dispData);							// 7Segに表示
+			_delay_ms(2000);								// 2秒待つ
+		}
     }
+	return 0;
 }
-
