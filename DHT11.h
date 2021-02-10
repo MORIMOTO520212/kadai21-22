6e3e0e1360e91d8c52210cdf6e2ec314761065a5
MorimotoYuma <kanten520212@gmail.com>
2021-02-10 20:48:41 +0900

    2021.02.10

--- DHT11.h
+++ DHT11.h
@@ -59,7 +59,6 @@ class DHT11
 	UCHR	temperature_L;						// 温度データ（小数点未満）
 	UCHR	humidity_H;							// 湿度データ（小数点以上）
 	UCHR	humidity_L;							// 湿度データ（小数点未満）
-	UCHR    temperature;
   public:
 	DHT11(void);								// コンストラクタ
 	int		DHT11Read( void);					// 温湿度データ受信メソッド
